5d66d33edc2dd00a921cc431d8ca8cc4b3ceab31
changjiashuai <changjiashuai@gmail.com>
2015-11-20 00:39:21 +0800

    Initial Commit

---
+++ NSTableViewTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSTableViewTutorial
+//
+//  Created by CJS on 15/11/20.
+//  Copyright © 2015年 CJS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
